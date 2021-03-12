fb6689faeb1882aea9d7c4c5f4a9b45c6eb406f2
Jeremy Wood <js.wood206@gmail.com>
2021-03-11 21:49:57 -0800

    commit tree 45453991c36495faffe3d0eff2e5b20080d2052a

--- index.js
+++ index.js
@@ -12,5 +12,8 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function(tutorial) {
+    tutorial = tutorial.split(" ").map(word => word[0].toUpperCase() + word.slice(1)).join(" ");
+    return tutorial;
+  });
 }
